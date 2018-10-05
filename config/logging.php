c92a6328efa85f2d8df49dfc961314bbe882c57b
sirbobz <bobcertified@gmail.com>
2018-10-05 16:15:22 +0300

    set the login param

--- config/logging.php
+++ config/logging.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/application.log'),
             'level' => 'debug',
             'days' => 14,
         ],
