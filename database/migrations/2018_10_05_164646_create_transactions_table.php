14eb9081ac0393ccf5f3391ddc7a763a7232d1b4
sirbobz <bobcertified@gmail.com>
2018-10-05 17:08:21 +0300

    create transactions table migration

---
+++ database/migrations/2018_10_05_164646_create_transactions_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('trx_id')->index()->unique();
+            $table->string('name')->nullable();
+            $table->string('acronym')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('url')->nullable();
+            $table->string('type'))->nullable();
+            $table->longText('languagesupported')->nullable();
+            $table->string('periodicity')->nullable();
+            $table->longText('economycoverage')->nullable();
+            $table->longText('granularity')->nullable();
+            $table->longText('numberofeconomies')->nullable();
+            $table->longText('topics')->nullable();
+            $table->longText('updatefrequency')->nullable();
+            $table->longText('updateschedule')->nullable();
+            $table->string('lastrevisiondate')->nullable();
+            $table->email('contactdetails')->nullable();
+            $table->longText('accessoption')->nullable();
+            $table->longText('bulkdownload')->nullable();
+            $table->longText('cite')->nullable();
+            $table->longText('detailpageurl')->nullable();
+            $table->string('popularity')->nullable();
+            $table->string('coverage')->nullable();
+            $table->string('api')->nullable();
+            $table->longText('apiaccessurl')->nullable();
+            $table->string('apisourceid')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
