e9e57452f187501d3987ef167a61bdc3ff4a054a
sirbobz <bobcertified@gmail.com>
2018-10-08 09:01:09 +0300

    created the display view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,41 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="table-responsive">
+                      <table id="data" class="table table-striped table no-margin">
+                        <thead>
+                          <tr class="" style="background-color:#C0C0C0;">
+                            <th class="text-center"> ID </th>
+                            <th class="text-center"> Name </th>
+                            <th class="text-center"> Acronym</th>
+                            <th class="text-center"> Description</th>
+                            <th class="text-center"> Type</th>
+                            <th class="text-center"> Created</th>
+                            <th class="text-center"> Updated</th>                    
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <!--  Initialize Table ID counter -->
+                          @php $id = 1; @endphp
+                          @foreach($data as $detail)
+                          <tr>
+                            <td class="text-center"> {{$id ++}} </td>
+                            <td class="text-center"> {{$detail->name}}</td>
+                            <td class="text-center"> {{$detail->acronym}} </td>
+                            <td class="text-center"> {{$detail->description}} </td>
+                            <td class="text-center"> {{$detail->type}} </td>
+                            <td class="text-center"> {{$detail->created_at}}</td>
+                            <td class="text-center"> {{$detail->updated_at}}</td>                             
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                    </div>
                 </div>
             </div>
         </div>
